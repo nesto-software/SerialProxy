f720ea434a263b828d9087215f8ef778f40556de
Martin Löper <martin.loeper@nesto-software.de>
2021-03-11 02:10:38 +0100

    feat: add new cross-toolchain to gh workflow

---
+++ scripts/install-cross-toolchain-armhf.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+FILE=/tmp/crosstool-ng-armhf.tar.xz
+
+apt-get install -y curl wget xz-utils
+
+mkdir -p /opt/crosstool-ng/x-tools/
+
+echo "Downloading crosstool-ng toolchain for armhf..."
+curl -s https://api.github.com/repos/nesto-software/crosstool-NG/releases/latest \
+| grep "crosstool-ng-armhf.tar.xz" \
+| cut -d : -f 2,3 \
+| tr -d \" \
+| wget -qi - -O "$FILE"
+
+tar xfv /tmp/crosstool-ng-armhf.tar.xz -C /opt/crosstool-ng/x-tools/
