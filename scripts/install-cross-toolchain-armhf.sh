ca49453a479892d5ccc5e26a4df31ded1c1a7bb6
Martin Löper <martin.loeper@nesto-software.de>
2021-03-11 02:35:14 +0100

    fix: untar

--- scripts/install-cross-toolchain-armhf.sh
+++ scripts/install-cross-toolchain-armhf.sh
@@ -3,15 +3,15 @@ set -e
 
 FILE=/tmp/crosstool-ng-armhf.tar.xz
 
-apt-get install -y curl wget xz-utils
+apt-get install -y curl wget xz-utils tar
 
-mkdir -p /opt/crosstool-ng/x-tools/
+sudo mkdir -p /opt/crosstool-ng/x-tools/
 
 echo "Downloading crosstool-ng toolchain for armhf..."
-curl https://api.github.com/repos/nesto-software/crosstool-NG/releases/latest \
+curl -s https://api.github.com/repos/nesto-software/crosstool-NG/releases/latest \
 | grep "crosstool-ng-armhf.tar.xz" \
 | cut -d : -f 2,3 \
 | tr -d \" \
-| wget -i - -O "$FILE"
+| wget -qi - -O "$FILE"
 
-sudo tar xfv /tmp/crosstool-ng-armhf.tar.xz -C /opt/crosstool-ng/x-tools/
+sudo tar xfv "$FILE" -C /opt/crosstool-ng/x-tools/
