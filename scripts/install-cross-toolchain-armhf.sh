d7b65b1724a508b4c5445def6bd05a5253c77ad8
Martin Löper <martin.loeper@nesto-software.de>
2021-03-11 02:32:50 +0100

    debug: wget error

--- scripts/install-cross-toolchain-armhf.sh
+++ scripts/install-cross-toolchain-armhf.sh
@@ -12,6 +12,6 @@ curl https://api.github.com/repos/nesto-software/crosstool-NG/releases/latest \
 | grep "crosstool-ng-armhf.tar.xz" \
 | cut -d : -f 2,3 \
 | tr -d \" \
-| wget -qi - -O "$FILE"
+| wget -i - -O "$FILE"
 
 sudo tar xfv /tmp/crosstool-ng-armhf.tar.xz -C /opt/crosstool-ng/x-tools/
